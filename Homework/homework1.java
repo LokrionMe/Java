382229169ccbc541b9b7a94f142ec40ca1a6c3ad
Lokrion <m.kostian@yandex.ru>
2022-12-14 23:55:24 +0700

    homework 2.1

--- Homework/homework1.java
+++ Homework/homework1.java
@@ -10,10 +10,13 @@ public class homework1 {
         // fact();
         // simple_numbers();
         // calc();
-        /* equ(); - оказался выше моего понимания, 
-        с некоторыми вариантами справляется(?7 + 6? = 83), но некоторые нет(6? + ?7 = 83),
-        почему так происходит я понимаю, но как с такими случаями справиться ума не приложу
-        */
+        /*
+         * equ(); - оказался выше моего понимания,
+         * с некоторыми вариантами справляется(?7 + 6? = 83), но некоторые нет(6? + ?7 =
+         * 83),
+         * почему так происходит я понимаю, но как с такими случаями справиться ума не
+         * приложу
+         */
         input_formul();
     }
 
@@ -143,36 +146,39 @@ public class homework1 {
             arr[i] = new StringBuilder(arr[i]).reverse().toString();
         }
         int k = 0;
-        for (int i = arr.length-1; i != -1 ; i--) {
+        for (int i = arr.length - 1; i != -1; i--) {
             String a = arr[i];
-            if (a.indexOf('?') != -1){
-                for (int j = 0; j < a.length() ; j++) {
+            if (a.indexOf('?') != -1) {
+                for (int j = 0; j < a.length(); j++) {
                     int res = 0;
-                    if (a.charAt(j) == '?'){
-                        if (i == 0){
-                            res = Character.getNumericValue(arr[2].charAt(j)) - Character.getNumericValue(arr[1].charAt(j));
-                        } else if (i == 1){
-                            res = Character.getNumericValue(arr[2].charAt(j)) - Character.getNumericValue(arr[0].charAt(j));
+                    if (a.charAt(j) == '?') {
+                        if (i == 0) {
+                            res = Character.getNumericValue(arr[2].charAt(j))
+                                    - Character.getNumericValue(arr[1].charAt(j));
+                        } else if (i == 1) {
+                            res = Character.getNumericValue(arr[2].charAt(j))
+                                    - Character.getNumericValue(arr[0].charAt(j));
                         } else {
-                            res = Character.getNumericValue(arr[0].charAt(j)) + Character.getNumericValue(arr[1].charAt(j));
+                            res = Character.getNumericValue(arr[0].charAt(j))
+                                    + Character.getNumericValue(arr[1].charAt(j));
                         }
-                        if (k==1){
-                            if (i==0 || i ==1){
-                                res-=1;
+                        if (k == 1) {
+                            if (i == 0 || i == 1) {
+                                res -= 1;
                             } else {
-                                res +=1;
+                                res += 1;
                             }
-                            k-=1;
+                            k -= 1;
                         }
-                        if (res<0){
-                            res+=10;
-                            k+=1;
-                        } else if (res>=10){
-                            res-=10;
-                            k+=1;
+                        if (res < 0) {
+                            res += 10;
+                            k += 1;
+                        } else if (res >= 10) {
+                            res -= 10;
+                            k += 1;
                         }
                         a = a.replace("?", Integer.toString(res));
-                    }                    
+                    }
                 }
                 arr[i] = a;
             }
@@ -180,10 +186,9 @@ public class homework1 {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = new StringBuilder(arr[i]).reverse().toString();
         }
-        if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) == Integer.parseInt(arr[2])){
-            System.out.println(arr[0]+" + "+arr[1]+" = "+arr[2]);
-        }
-        else{
+        if (Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]) == Integer.parseInt(arr[2])) {
+            System.out.println(arr[0] + " + " + arr[1] + " = " + arr[2]);
+        } else {
             System.out.println("No variants");
         }
     }
@@ -198,20 +203,21 @@ public class homework1 {
         int res = 0;
         int k = 1;
         for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1){
-                res += InputNumber("Input number of "+ arr[i] + " =", k);
+            if (i == arr.length - 1) {
+                res += InputNumber("Input number of " + arr[i] + " =", k);
             } else {
-                if (arr[i].equals(arr[i+1])){
-                    k+=1;
+                if (arr[i].equals(arr[i + 1])) {
+                    k += 1;
                 } else {
-                    res += InputNumber("Input number of "+ arr[i] + " =", k);
-                    k = 1;   
+                    res += InputNumber("Input number of " + arr[i] + " =", k);
+                    k = 1;
                 }
             }
-            
+
         }
         System.out.println(res);
     }
+
     private static Integer InputNumber(String line, Integer key) {
         int numb = 0;
         Scanner scanner = new Scanner(System.in);
@@ -220,9 +226,9 @@ public class homework1 {
             scanner = new Scanner(System.in);
             if (scanner.hasNextInt()) {
                 numb = scanner.nextInt();
-                return numb*key;
+                return numb * key;
             } else {
-                 System.out.println("It must be a number");
+                System.out.println("It must be a number");
             }
         }
     }
